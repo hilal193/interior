9365623817e0c5aa3f154b8e486feaef7d97b881
Hilal <hilal1993@hotmail.be>
2022-03-01 10:24:14 +0100

    integration part 3

---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,21 @@
+
+
+
+<header id="header" id="home">
+    <div class="container main-menu">
+        <div class="row align-items-center justify-content-between d-flex">
+            <div id="logo">
+                <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+            </div>
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="menu-active"><a href="index.html">Home</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="projects.html">Projects</a></li>
+                    <li><a href="blog-home.html">Blog</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav><!-- #nav-menu-container -->
+        </div>
+    </div>
+</header><!-- #header -->
